a3d356adab9ea20111c2e2436b8b42a34d7bef8f
Adrian Koša <adriankosa13@gmail.com>
2022-05-13 08:16:04 +0200

    WIP add redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 100,
+    title: 'cellphone',
+    description: 'Xiaomi cellphone'
+  },
+  {
+    id: 'p2',
+    price: 10,
+    title: 'headphones',
+    description: 'Wireless headphones'
+  }
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product =>
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />)}
       </ul>
     </section>
   );
